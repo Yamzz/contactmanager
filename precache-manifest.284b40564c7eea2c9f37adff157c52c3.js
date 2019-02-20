799ceeb620602cfc4a6df7ec887ebaba6eeed888
Mr Yamz <Inyambae@hotmail.com>
2019-02-20 15:07:26 +0000

    Updates

---
+++ precache-manifest.284b40564c7eea2c9f37adff157c52c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "8b738c518780b97a3305",
+    "url": "/contactmanager/static/js/main.8b738c51.chunk.js"
+  },
+  {
+    "revision": "a465d015f017d40ebd1e",
+    "url": "/contactmanager/static/js/2.a465d015.chunk.js"
+  },
+  {
+    "revision": "8b738c518780b97a3305",
+    "url": "/contactmanager/static/css/main.b0740dc4.chunk.css"
+  },
+  {
+    "revision": "a465d015f017d40ebd1e",
+    "url": "/contactmanager/static/css/2.c1ace40b.chunk.css"
+  },
+  {
+    "revision": "a28b30a980971d6c25b95617138e063c",
+    "url": "/contactmanager/index.html"
+  }
+];
